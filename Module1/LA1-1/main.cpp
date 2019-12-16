140f670b20abc9583dc861e9503c23031f502bff
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 09:53:09 -0700

    this the second time ill commit to this

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout <<" Now inside VSCode and GITHub" <<std::endl;
+int age; // int: signed integers
+
+    //cout: for system output
+    cout <<" Now inside VSCode and GITHub" <<endl;
+    cout << "another line under namespace std" << endl;
+    cout << " wildcats "<<" WSU"<<" Ogden "<<endl;
+    // cin: for System/user input
+    cout << "How old are you in years?" << endl;
+    cin >> age;
+    cout << "i see you are: " << age << endl;
+    
     return 0;
 }
